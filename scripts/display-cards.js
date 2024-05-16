ea15b6c9b48a96b83d862e5fd36c6c0e69a495ad
Ember <burningembersemail@gmail.com>
2024-05-16 13:44:38 -0400

    Made an actual website
    The printable version of the cards can be accessed
    via the "Print Cards" button. The site now includes
    rules, credits, and a link back to the github.
    
    Ember <3

---
+++ scripts/display-cards.js
@@ -0,0 +1,63 @@
+const backing = `
+<div class="page">
+  <div class="backing">
+    <div>
+      ${(() => {
+        let result = "";
+        for (let i = 0; i < 20; i++) {
+          if (i % 2 == 1) {
+            result += "           ";
+          }
+          for (let i = 0; i < 7; i++) {
+            result += "Bangarang      ";
+          }
+          result += "\n\n";
+        }
+        return result;
+      })()}
+    </div>
+  </div>
+</div>
+`;
+
+async function generateCards() {
+  const cardData = await parseCards("data/example.csv", 0);
+  const fancyborder = true;
+  const doBackground = true;
+  const doBacks = true;
+  const cardsPerPage = 9;
+
+  for (let i = 0; i < cardData.length; i += cardsPerPage) {
+    // Construct one page at a time
+    // TODO: use a grid rather than a flexbox for the cards
+    let cardHTML = "";
+    for (let j = 0; j < cardsPerPage && i + j < cardData.length; ++j) {
+      const card = cardData[i + j];
+      // TODO: avoid diretly parsing questions as html - this is a security issue
+      cardHTML += `
+        <div class="card ${fancyborder ? "fancyborder" : ""}">
+          <div class="questions">
+            ${card.questions.map((question) => `<div>${question}</div>`).join("\n")}
+          </div>
+          <div class="bottom">
+            ${card.bottomText}
+          </div>
+        </div>
+      `;
+    }
+    let cardPage = `
+      <div class="page">
+        <div class="cards">
+          ${doBackground ? '<img src="images/background-color.png">' : ""}
+          <div> ${cardHTML} </div>
+        </div>
+      </div>
+    `;
+    document.body.insertAdjacentHTML("beforeend", cardPage);
+    if (doBacks) {
+      document.body.insertAdjacentHTML("beforeend", backing);
+    }
+  }
+}
+
+generateCards();
