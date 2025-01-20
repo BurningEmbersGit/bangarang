c8f415b82aac57d2794b64e0b3d66ed6f74866a1
Ember <burningembersemail@gmail.com>
2025-01-20 17:51:58 -0500

    Swap from *bold* to *italic*

--- scripts/parse-cards.js
+++ scripts/parse-cards.js
@@ -1,7 +1,7 @@
 function qTransforms(question) {
   question = question.replace("(tf)", "<b>True or False:</b>");
   question = question.replace("(wr)", "<b>Would I Rather:</b>");
-  question = question.replace(/\*[^\*]*\*/g, "<b>$&</b>").replace(/\*/g, "");
+  question = question.replace(/\*[^\*]*\*/g, "<i>$&</i>").replace(/\*/g, "");
   question = question.replace(/''/g, '"');
   question = question.replace("\\;", ",");
   return question;
